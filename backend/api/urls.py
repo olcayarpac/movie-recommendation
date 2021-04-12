4afbab0d881bf965f0584ffaf2ad5efba3bb9c20
Olcay Arpac <olcayarpac@yahoo.com>
2021-04-12 14:22:47 +0300

    login and singup functions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.signup, name='index'),
+    path('signup', views.signup, name='singup'),
+    path('login', views.login, name='login'),
+
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 import json
 import pymongo
 
+# TODO: giriş yap ve kaydol fonksiyonlarını düzgünce geliştir
+# TODO: kaydol fonksiyonu username ve email kontrolü yapacak
+# TODO: kayıt olma veya giriş yapma başarılı olduğunda userid döndüreceğiz
+# TODO: ilerleyen zamanlarda userid yerine token kontrolü yapılabilir
+
 def bodyToJson(reqBody):
   bodyJson = {}
   for param in reqBody.split('&'):
@@ -14,10 +19,34 @@ def signup(request):
   req_body = request.body.decode('utf-8')
   userJson = bodyToJson(req_body)
 
-  myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  myclient = pymongo.MongoClient('mongodb://localhost:27017/')
   mydb = myclient['moviedb']
   usersCol = mydb['users']
 
-  usersCol.insert_one(userJson)
+  if not usersCol.count({ '$or': [{'username': userJson['username']}, {'email': userJson['email']}]}) == 0:
+    print('Already exist')
+    myclient.close()
+    return HttpResponse('Username or email already taken', status=409)
+  else:
+    newUser = usersCol.insert(userJson)
+    myclient.close()
+    response = HttpResponse("Sign Up Succesful", status=200)
+    response.set_cookie('id', str(newUser))
+    return response
 
-  return HttpResponse("Hello world")
+
+def login(request):
+  req_body = request.body.decode('utf-8')
+  userJson = bodyToJson(req_body)
+
+  myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+  mydb = myclient['moviedb']
+  usersCol = mydb['users']
+
+  cursor = list(usersCol.find({ '$and': [{'username': userJson['username']}, {'password': userJson['password']}]}, {'_id':1}))
+  if not cursor:
+    return HttpResponse('Invalid username or password', status=401)
+  else:
+    response = HttpResponse('Login Successful', status=200)
+    response.set_cookie('id', str(cursor[0]['_id'])) 
+    return response
